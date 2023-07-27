0d2e6c9e26df46350b9a77e741038537b95672f5
Kel-VinTech <mondaykelvintech@gmail.com>
2023-07-27 19:13:55 -0400

    made changes

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -41,7 +41,7 @@ contract Raffle {
 
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
-    address private immutable i_vrfCoordinator;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     bytes32 private immutable i_gasLane;
     uint64 private immutable i_subscriptionId;
     uint32 private immutable i_callbackGasLimit;
@@ -70,7 +70,7 @@ contract Raffle {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
-        i_vrfCoordinator = vrfCoordinator;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
