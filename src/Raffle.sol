fb070f5b77d2b75cd73418ef697bdbc92a063131
Kel-VinTech <mondaykelvintech@gmail.com>
2023-07-30 19:40:37 -0400

    added codes

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -24,6 +24,7 @@
 pragma solidity ^0.8.19;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 /**
  * @title A sample Raffle Contract
@@ -92,6 +93,11 @@ contract Raffle {
             NUMWORDS
         );
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uit256[] memory randomWords
+    ) internal override {}
 
     // Getters functions
 
